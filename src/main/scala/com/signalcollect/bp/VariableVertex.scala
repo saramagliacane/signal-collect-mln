c63fcc10d7b7f5cc7b77f200dbc7df798dac097e
Philip Stutz <philip.stutz@gmail.com>
2014-07-16 08:56:54 +0200

    Don't remember. :P

--- src/main/scala/com/signalcollect/bp/VariableVertex.scala
+++ src/main/scala/com/signalcollect/bp/VariableVertex.scala
@@ -1,18 +1,37 @@
 package com.signalcollect.bp
 
 import com.signalcollect._
+import scala.collection.parallel.immutable.WithDefault
 
 /**
  * The vertex representing the variables.
  * @param id
  * @param initialState
  */
-class LogVariableVertex(id: String,
-                        initialState: Distribution,
-                        val isEvidence: Boolean)
+class LogVariableVertex(
+  id: String,
+  initialState: Map[String, Distribution] = Map.withDefaultValue(JoinIdentity),
+  val isEvidence: Boolean)
     extends DataGraphVertex(id, initialState) {
   type Signal = Distribution
-  def collect = signals.reduce(_ + _)
+  def collect = {
+    val left = signals.toArray
+    val right = left.reverse
+    //    for (i <- (0 to left.length)) {
+    //      
+    //    }
+    var acc: Distribution = JoinIdentity
+    val accFromLeft = left map { signal =>
+      acc = signal * acc
+      acc
+    }
+    acc = JoinIdentity
+    val accFromLeft = left map { signal =>
+      acc = signal * acc
+      acc
+    }
+    signals.reduce(_ + _)
+  }
   def signalMap = mostRecentSignalMap
 }
 
